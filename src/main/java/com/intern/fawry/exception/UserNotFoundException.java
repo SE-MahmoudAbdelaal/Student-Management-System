bc4d5b20454fea0bbd544b871797b7fa7d3caff3
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-07-03 08:29:19 +0300

    Add UserNotFoundException class for handling user not found exceptions

---
+++ src/main/java/com/intern/fawry/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.intern.fawry.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
