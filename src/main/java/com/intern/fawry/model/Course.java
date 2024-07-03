a9f96bbf2c5264837bacc238f42248366da24b29
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-07-03 08:26:38 +0300

    Add Course model class with lombok annotations

---
+++ src/main/java/com/intern/fawry/model/Course.java
@@ -0,0 +1,14 @@
+package com.intern.fawry.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    private int id;
+    private String name;
+    private String description;
+}
