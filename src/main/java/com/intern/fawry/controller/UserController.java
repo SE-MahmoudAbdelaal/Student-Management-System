ebac487ecb9e5cbc4c92406c7df9c5fca2324d89
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-07-03 08:24:01 +0300

    Add UserController for managing CRUD operations on User entities

---
+++ src/main/java/com/intern/fawry/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.intern.fawry.controller;
+
+import com.intern.fawry.model.User;
+import com.intern.fawry.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+       return  userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User user) {
+        userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public void updateUser(@PathVariable int  id, @RequestBody User user) {
+        userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+    }
+}
