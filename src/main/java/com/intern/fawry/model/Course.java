325881043227918c3b1b200a8156eb41ce47e79a
mohammed-ZK <git@github.com:mohammed-ZK/e-commerc.git>
2024-07-03 12:13:37 +0300

    add validation and excption handling

--- src/main/java/com/intern/fawry/model/Course.java
+++ src/main/java/com/intern/fawry/model/Course.java
@@ -1,7 +1,5 @@
 package com.intern.fawry.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Course {
     private int id;
-    @NotBlank(message = "the name is required")
     private String name;
-    @NotNull(message = "the description is required")
     private String description;
 }
